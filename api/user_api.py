69071f595cddcb1965c1598702b36e42d62c96a7
Daniel Leitner <DaniLeit@seznam.cz>
2026-04-03 17:14:33 +0200

    add get_users helper function

--- api/user_api.py
+++ api/user_api.py
@@ -7,3 +7,10 @@ def create_user(base_url, payload):
     response = requests.post(base_url, json=payload, timeout=3)
     data = response.json()
     return response, data
+
+
+def get_users(base_url):
+
+    response = requests.get(base_url, timeout=3)
+    data = response.json()
+    return response, data
