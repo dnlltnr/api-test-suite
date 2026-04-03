5d2fea75bc964e0e30f8a8da006465795ccc5d7d
Daniel Leitner <DaniLeit@seznam.cz>
2026-04-03 17:23:44 +0200

    add test_create_user_valid function

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,3 +7,18 @@ from api.user_api import create_user, get_users
 def base_url():
     return "https://jsonplaceholder.typicode.com/users"
 
+
+def test_create_user_valid(base_url):
+
+    response, data = create_user(base_url, {
+        "name": "Daniel",
+        "email": "dan@test.com"
+    })
+
+    assert response.status_code == 201
+    assert "id" in data
+    assert "name" in data
+    assert "email" in data
+    assert data["name"] == "Daniel"
+    assert data["email"] == "dan@test.com"
+
